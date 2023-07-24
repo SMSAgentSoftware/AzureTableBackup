27f1c559b6393d815e4f78843930d9621fb54910
Trevor Jones <SMSAgentSoftware@users.noreply.github.com>
2023-07-24 14:56:39 -0300

    Final production version of the resource deployment script

--- Deploy-AzureTableBackupSolution.ps1
+++ Deploy-AzureTableBackupSolution.ps1
@@ -2,14 +2,15 @@
 ## Deploys and configures the Azure resources required by the Azure Table Backup solution ##
 ############################################################################################
 
-#! Run this script with the Owner or Contributer role in the Azure subscription !#
+#! Run this script with the [Owner] or [Contributer + User Access Administrator] roles in the Azure subscription !#
 
 ## Check required modules
 #Requires -Modules Az.Accounts,Az.Resources,Az.Storage,Az.OperationalInsights,Az.ApplicationInsights,Az.Functions,Az.WebSites
 
 ### VARIABLES TO SET
-$Subscription = "<MyAzureSubscription>" # The name of the Azure subscription which will host your resources
-$Location = "<Location>" # The Azure region for your resources. Find available regions like so: Get-AzLocation | Where {$_.RegionType -eq "Physical"} | Select -ExpandProperty DisplayName | Sort, or here: https://azure.microsoft.com/en-us/explore/global-infrastructure/data-residency/#select-geography
+$Tenant = "<Tenant>"# The ID of the tenant containing your Azure subscription
+$Subscription = "<Subscription>" # The name of the Azure subscription which will host your resources
+$Location = "East US" # The Azure region for your resources. Find available regions like so: Get-AzLocation | Where {$_.RegionType -eq "Physical"} | Select -ExpandProperty DisplayName | Sort, or here: https://azure.microsoft.com/en-us/explore/global-infrastructure/data-residency/#select-geography
 ###
 
 ### RESOURCE NAMES
@@ -19,10 +20,16 @@ $StorageAccountName = "staztablebackup$($RandomSuffix)"
 $LogAnalyticsWorkspaceName = "log-azTableBackup-$($RandomSuffix)"
 $ApplicationInsightsName = "appi-azTableBackup-$($RandomSuffix)"
 $FunctionAppName = "func-azTableBackup-$($RandomSuffix)"
+$BackupConfigurationTableName = 'AutomatedTableBackupConfiguration'
+$BackupContainerName = 'tablebackups'
 
 # Check Azure region availability
 $UnavailableRegions = @{
-    "China East" = "Azure Functions"
+    "China East" = "Azure Functions, Application Insights, Log Analytics"
+    "China Non-Regional" = "Azure Function, Application Insights, Log Analytics, Storage Accounts"
+    "China North" = "Application Insights, Log Analytics"
+    "China North2" = "Application Insights, Log Analytics"
+    "West Central US" = "Application Insights"
 }
 
 If ($UnavailableRegions.Keys -contains $Location)
@@ -32,7 +39,42 @@ If ($UnavailableRegions.Keys -contains $Location)
 }
 
 # Connect to Azure AD
-$Connection = Connect-AzAccount -Subscription $Subscription
+try 
+{
+    $Connection = Connect-AzAccount -Subscription $Subscription -Tenant $Tenant -ErrorAction Stop
+}
+catch 
+{
+    throw $_.Exception.Message
+}
+
+# Check role assignments
+try 
+{
+    $RoleAssignments = Get-AzRoleAssignment `
+        -SignInName $Connection.Context.Account.Id `
+        -Scope "/subscriptions/$($Connection.Context.Subscription.id)" `
+        -IncludeClassicAdministrators `
+        -ErrorAction Stop |
+        Where {$_.Scope -eq "/subscriptions/$($Connection.Context.Subscription.id)"}
+
+    If ($RoleAssignments.Count -ge 1)
+    {
+        [array]$Roles = $RoleAssignments.RoleDefinitionName
+        If (-not ($Roles -contains "ServiceAdministrator" -or $Roles -contains "CoAdministrator" -or $Roles -contains "Owner" -or ($Roles -contains "Contributor" -and $Roles -contains "User Access Administrator")))
+        {
+            throw "The signed-in user does not have the required role assignments in this subscription. Either the 'Owner' role or the 'Contributor' PLUS 'User Access Administrator' roles are required"
+        }
+    }
+    else 
+    {
+        throw "The signed-in user does not have the required role assignments in this subscription. Either the 'Owner' role or the 'Contributor' PLUS 'User Access Administrator' roles are required"    
+    }
+}
+catch 
+{
+    throw $_.Exception.Message
+}
 
 # Create resource group
 Write-Host "Creating resource group..." -NoNewline
@@ -45,7 +87,7 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Error $_
+    Write-Error $_.Exception.Message.Split([Environment]::NewLine)[0]
     return
 }
 
@@ -71,7 +113,7 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Error -Message $_.Exception.Message
+    Write-Error -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
     return
 }
 
@@ -89,7 +131,7 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Warning $_.Exception.Message
+    Write-Warning $_.Exception.Message.Split([Environment]::NewLine)[0]
 }
 
 # Enable blob soft delete
@@ -106,7 +148,7 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Warning $_.Exception.Message
+    Write-Warning $_.Exception.Message.Split([Environment]::NewLine)[0]
 }
 
 # Create a storage context
@@ -121,45 +163,8 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Error -Message $_.Exception.Message
+    Write-Error -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
     return
-}
-
-# Create backup configuration table
-Write-Host "Creating a backup configuration table..." -NoNewline
-$TableName = 'AutomatedTableBackupConfiguration'
-Write-Host "$TableName..." -NoNewline
-try 
-{
-    $BackupConfigurationTable = New-AzStorageTable `
-        -Name "$TableName" `
-        -Context $StorageContext `
-        -ErrorAction Stop
-    Write-Host "Success!" -ForegroundColor Green
-}
-catch 
-{
-    Write-Host "Failed!" -ForegroundColor Red
-    Write-Warning -Message $_.Exception.Message
-}
-
-# Create backup container
-Write-Host "Creating a backup container..." -NoNewline
-$ContainerName = 'tablebackups'
-Write-Host "$ContainerName..." -NoNewline
-try 
-{
-    $BackupConfigurationContainer = New-AzStorageContainer `
-        -Name "$ContainerName" `
-        -Context $StorageContext `
-        -Permission Off `
-        -ErrorAction Stop
-    Write-Host "Success!" -ForegroundColor Green
-}
-catch 
-{
-    Write-Host "Failed!" -ForegroundColor Red
-    Write-Warning -Message $_.Exception.Message
 }
 
 # Create a log analytics workspace
@@ -177,7 +182,7 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Error -Message $_.Exception.Message
+    Write-Error -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
     return
 }
 
@@ -197,7 +202,7 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Error -Message $_.Exception.Message
+    Write-Error -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
     return
 }
 
@@ -205,7 +210,7 @@ catch
 Write-Host "Creating a function app..." -NoNewline
 $AppSettings = @{
     'BackupConfigurationStorageAccount' = $StorageAccountName
-    'BackupConfigurationStorageTable' = $TableName
+    'BackupConfigurationStorageTable' = $BackupConfigurationTableName
     'BackupConfigurationTimerExpression' = "0 0 1 * * *"
     'WEBSITE_RUN_FROM_PACKAGE' = "1"
 }
@@ -230,35 +235,38 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Error -Message $_.Exception.Message
+    Write-Error -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
+    return
+}
+
+# Add portal.azure.com to CORS
+Write-Host "Adding 'portal.azure.com' to CORS on function app..." -NoNewline
+try 
+{
+    $AzResourceParams = @{
+        ResourceGroupName = $ResourceGroupName
+        ResourceName = $FunctionAppName
+        ResourceType =  "Microsoft.Web/sites"
+    }
+    $WebAppResource = Get-AzResource @AzResourceParams -ErrorAction Stop
+    $WebAppResource.Properties.siteConfig.cors = @{
+        allowedOrigins = @("https://portal.azure.com")
+    }
+    $Update = $WebAppResource | Set-AzResource -Force -ErrorAction Stop
+    Write-Host "Success!" -ForegroundColor Green
+}
+catch 
+{
+    Write-Host "Failed!" -ForegroundColor Red
+    Write-Error -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
     return
 }
 
 # Assign Azure roles
 # note the required permissions
 # https://learn.microsoft.com/en-us/azure/role-based-access-control/role-assignments-powershell#prerequisites
-Write-Host "Adding role assignments for function app system identity to storage account..."
-"Storage Table Data Reader","Storage Blob Data Contributor" | foreach {
-    Write-Host "  $_..." -NoNewline
-    try 
-    {
-        $RoleAssignment = New-AzRoleAssignment `
-        -ObjectId $FunctionApp.IdentityPrincipalId `
-        -RoleDefinitionName "$_" `
-        -Scope $StorageAccount.Id `
-        -WarningAction SilentlyContinue `
-        -ErrorAction Stop
-        Write-Host "Success!" -ForegroundColor Green
-    }
-    catch 
-    {
-        Write-Host "Failed!" -ForegroundColor Red
-        Write-Warning -Message $_.Exception.Message
-    }
-}
-
-Write-Host "Adding role assignment for current user to storage account..."
-"Storage Table Data Contributor"| foreach {
+Write-Host "Adding role assignments for current user to storage account..."
+"Storage Table Data Contributor","Storage Blob Data Contributor" | foreach {
     Write-Host "  $_..." -NoNewline
     try 
     {
@@ -275,8 +283,64 @@ Write-Host "Adding role assignment for current user to storage account..."
     catch 
     {
         Write-Host "Failed!" -ForegroundColor Red
-        Write-Warning -Message $_.Exception.Message
+        Write-Warning -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
     }
+}
+
+Write-Host "Adding role assignments for function app system identity to storage account..."
+"Storage Table Data Reader","Storage Blob Data Contributor" | foreach {
+    Write-Host "  $_..." -NoNewline
+    try 
+    {
+        $RoleAssignment = New-AzRoleAssignment `
+        -ObjectId $FunctionApp.IdentityPrincipalId `
+        -RoleDefinitionName "$_" `
+        -Scope $StorageAccount.Id `
+        -WarningAction SilentlyContinue `
+        -ErrorAction Stop
+        Write-Host "Success!" -ForegroundColor Green
+    }
+    catch 
+    {
+        Write-Host "Failed!" -ForegroundColor Red
+        Write-Warning -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
+    }
+}
+
+# Create backup configuration table
+Write-Host "Creating a backup configuration table..." -NoNewline
+Write-Host "$BackupConfigurationTableName..." -NoNewline
+try 
+{
+    $BackupConfigurationTable = New-AzStorageTable `
+        -Name "$BackupConfigurationTableName" `
+        -Context $StorageContext `
+        -ErrorAction Stop
+    Write-Host "Success!" -ForegroundColor Green
+}
+catch 
+{
+    Write-Host "Failed!" -ForegroundColor Red
+    Write-Warning -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
+}
+
+# Create backup container
+Write-Host "Creating a backup container..." -NoNewline
+
+Write-Host "$BackupContainerName..." -NoNewline
+try 
+{
+    $BackupConfigurationContainer = New-AzStorageContainer `
+        -Name "$BackupContainerName" `
+        -Context $StorageContext `
+        -Permission Off `
+        -ErrorAction Stop
+    Write-Host "Success!" -ForegroundColor Green
+}
+catch 
+{
+    Write-Host "Failed!" -ForegroundColor Red
+    Write-Warning -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
 }
 
 # Add the backup configuration table itself to the backup
@@ -289,15 +353,15 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Warning -Message $_.Exception.Message
+    Write-Warning -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
 }
 
 if ($GetStorageToken)
 {
     $Body = @{
         PartitionKey = $StorageAccountName
-        RowKey = $ContainerName
-        SourceTableNames = "$TableName"
+        RowKey = $BackupContainerName
+        SourceTableNames = "$BackupConfigurationTableName"
     }
     $Headers = @{
         Accept = "application/json;odata=nometadata"
@@ -353,7 +417,5 @@ try
 catch 
 {
     Write-Host "Failed!" -ForegroundColor Red
-    Write-Warning -Message $_.Exception.Message
+    Write-Warning -Message $_.Exception.Message.Split([Environment]::NewLine)[0]
 }
-
-# Add https://portal.azure.com to function app CORS
